35f7671131f1cfcd5fa6264c7d754f8eccaae344
W. Matthew Mills <30300016+WMatthewARC@users.noreply.github.com>
2022-10-20 12:26:45 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,8 +5,6 @@ var defultMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-
-
 // gray scale layer
 var grayscale = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -21,7 +19,6 @@ let basemap= {
     Defult: defaultMap,
     Grayscale: grayscale
 };
-
 
 
 //make a map object
